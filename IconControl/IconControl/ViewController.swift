6daa380f9bbeac2f0f9ec15223e6596f3606f6dc
Sam Davies <sam@razeware.com>
2015-08-11 22:07:00 +0100

    Created new icon project

---
+++ IconControl/IconControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IconControl
+//
+//  Created by Sam Davies on 11/08/2015.
+//  Copyright © 2015 Razeware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
