8afcf33ea8db93a90ea6d5f1ce641e8b7ba436ad
Sam Davies <sam@razeware.com>
2015-08-12 15:34:35 +0100

    Created sample project for the drawing control

---
+++ SketchPad/SketchPad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SketchPad
+//
+//  Created by Sam Davies on 12/08/2015.
+//  Copyright © 2015 Razeware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
