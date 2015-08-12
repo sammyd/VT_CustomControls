89528c5b60e5367fac399eb85fd7ceabe2a9ba73
Sam Davies <sam@razeware.com>
2015-08-12 20:52:37 +0100

    Created basic canvas view

---
+++ SketchPad/SketchPad/Canvas.swift
@@ -0,0 +1,47 @@
+//
+//  Canvas.swift
+//  SketchPad
+//
+//  Created by Sam Davies on 12/08/2015.
+//  Copyright © 2015 Razeware. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class Canvas : UIView {
+  
+  private var drawing: UIImage?
+  
+  override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    if let touch = touches.first {
+      addLineFromPoint(touch.previousLocationInView(self), toPoint: touch.locationInView(self))
+    }
+  }
+}
+
+
+extension Canvas {
+  private func addLineFromPoint(from: CGPoint, toPoint: CGPoint) {
+    UIGraphicsBeginImageContextWithOptions(bounds.size, false, 0.0)
+    
+    drawing?.drawInRect(bounds)
+    
+    let cxt = UIGraphicsGetCurrentContext()
+    CGContextMoveToPoint(cxt, from.x, from.y)
+    CGContextAddLineToPoint(cxt, toPoint.x, toPoint.y)
+    
+    CGContextSetLineCap(cxt, .Round)
+    CGContextSetLineWidth(cxt, 4.0)
+    UIColor.blackColor().setStroke()
+    
+    CGContextStrokePath(cxt)
+    
+    drawing = UIGraphicsGetImageFromCurrentImageContext()
+    
+    layer.contents = drawing?.CGImage
+    
+    UIGraphicsEndImageContext()
+  }
+  
+}
