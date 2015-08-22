356dbccbfd125f77c90726f36bd39259640f5198
Sam Davies <sam@razeware.com>
2015-08-22 18:38:28 +0100

    Video 5: Completed building the challenge starter
    This includes the skeleton of the ThreeRingView view class

--- projects/05_AdvancedLayers/005_Challenge_Starter/HealthRings.playground/Contents.swift
+++ projects/05_AdvancedLayers/005_Challenge_Starter/HealthRings.playground/Contents.swift
@@ -2,10 +2,95 @@ import UIKit
 import XCPlayground
 
 
-let ring = RingLayer()
-ring.value = 1.6
-ring.ringWidth = 40
-ring.ringColors = (UIColor.hrPinkColor.CGColor, UIColor.hrPinkColor.darkerColor.CGColor)
+class ThreeRingView : UIView {
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    sharedInitialization()
+    
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    sharedInitialization()
+  }
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    drawLayers()
+  }
+  
+  
+  private func sharedInitialization() {
+    backgroundColor = UIColor.blackColor()
+  }
+  
+  private func drawLayers() {
+    
+  }
+  
+  //: API Properties
+  var ringWidth : CGFloat = 0.0
+  var ringPadding : CGFloat = 0.0
+  var ringBackgroundColor : UIColor = UIColor.darkGrayColor()
+}
 
-viewWithLayer(ring)
+//: Values
+extension ThreeRingView {
+  var innerRingValue : CGFloat {
+    get {
+      return 0
+    }
+    set(newValue) {
+      
+    }
+  }
+  var middleRingValue : CGFloat {
+    get {
+      return 0
+    }
+    set(newValue) {
+      
+    }
+  }
+  var outerRingValue : CGFloat {
+    get {
+      return 0
+    }
+    set(newValue) {
+      
+    }
+  }
+}
+
+//: Colors
+extension ThreeRingView {
+  var innerRingColor : UIColor {
+    get {
+      return UIColor.clearColor()
+    }
+    set(newColor) {
+      
+    }
+  }
+  var middleRingColor : UIColor {
+    get {
+      return UIColor.clearColor()
+    }
+    set(newColor) {
+      
+    }
+  }
+  var outerRingColor : UIColor {
+    get {
+      return UIColor.clearColor()
+    }
+    set(newColor) {
+      
+    }
+  }
+}
+
+let threeRings = ThreeRingView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+threeRings
 
