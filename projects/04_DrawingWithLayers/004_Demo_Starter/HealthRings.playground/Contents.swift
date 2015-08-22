b691b4dbdf5141010957616e6221da4ecc921138
Sam Davies <sam@razeware.com>
2015-08-22 09:45:50 +0100

    Video 4: Create starter playground

---
+++ projects/04_DrawingWithLayers/004_Demo_Starter/HealthRings.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+import XCPlayground
+
+public class RingLayer : CALayer {
+  
+  private let angleOffsetForZero = CGFloat(-M_PI_2)
+  
+  //:- Public API
+  var ringWidth: CGFloat = 20.0
+  var value: CGFloat = 0.7
+  var ringColors: (CGColorRef, CGColorRef) = (UIColor.redColor().CGColor, UIColor.blackColor().CGColor)
+  var ringBackgroundColor: CGColorRef = UIColor.darkGrayColor().CGColor
+  
+  //:- Initialisation
+  public override init() {
+    super.init()
+    sharedInitialization()
+  }
+  
+  required public init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    sharedInitialization()
+  }
+}
+
+extension RingLayer {
+  private func sharedInitialization() {
+    backgroundColor = UIColor.blackColor().CGColor
+  }
+}
+
+
+
+viewWithLayer(RingLayer())
+
