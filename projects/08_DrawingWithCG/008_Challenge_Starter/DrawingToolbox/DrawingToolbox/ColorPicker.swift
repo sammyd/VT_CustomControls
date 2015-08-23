c75cf3da7de3286af2b9a764166385309abab6ec
Sam Davies <sam@razeware.com>
2015-08-23 23:18:35 +0100

    Video 8: Created the challenge starter
    The challenge becomes to fill out the ColorRing class with CG code

--- projects/08_DrawingWithCG/008_Challenge_Starter/DrawingToolbox/DrawingToolbox/ColorPicker.swift
+++ projects/08_DrawingWithCG/008_Challenge_Starter/DrawingToolbox/DrawingToolbox/ColorPicker.swift
@@ -25,8 +25,6 @@ import UIKit
 @IBDesignable
 public class ColorPicker : UIControl {
   private var colorRing : ColorRing?
-  private var gestureRecognizer : AngleGestureRecognizer?
-  private var transformAtStartOfGesture : CGAffineTransform?
   
   public override init(frame: CGRect) {
     super.init(frame: frame)
@@ -53,39 +51,6 @@ public class ColorPicker : UIControl {
         colorRing.bottomAnchor.constraintEqualToAnchor(bottomAnchor)
       ]
     )
-    
-    let selectedColorView = UIView()
-    selectedColorView.translatesAutoresizingMaskIntoConstraints = false
-    selectedColorView.backgroundColor = UIColor.clearColor()
-    selectedColorView.layer.borderColor = UIColor(white: 0, alpha: 0.3).CGColor
-    selectedColorView.layer.borderWidth = 3.0
-    addSubview(selectedColorView)
-    NSLayoutConstraint.activateConstraints(
-      [
-        selectedColorView.centerXAnchor.constraintEqualToAnchor(centerXAnchor),
-        selectedColorView.topAnchor.constraintEqualToAnchor(topAnchor),
-        selectedColorView.widthAnchor.constraintEqualToAnchor(widthAnchor, multiplier: 0.05),
-        selectedColorView.heightAnchor.constraintEqualToAnchor(heightAnchor, multiplier: 0.3)
-      ]
-    )
-    
-    gestureRecognizer = AngleGestureRecognizer(target: self, action: "handleAngleGestureChange")
-    addGestureRecognizer(gestureRecognizer!)
-  }
-}
-
-extension ColorPicker {
-  func handleAngleGestureChange() {
-    switch gestureRecognizer!.state {
-    case UIGestureRecognizerState.Began:
-      transformAtStartOfGesture = colorRing?.transform
-    case .Changed:
-      colorRing?.transform = CGAffineTransformRotate(transformAtStartOfGesture!, gestureRecognizer!.angleDelta)
-    default:
-      transformAtStartOfGesture = .None
-    }
-    
-    sendActionsForControlEvents(.ValueChanged)
   }
 }
 
@@ -99,38 +64,6 @@ extension ColorPicker {
       return colorRing?.ringWidth ?? 0
     }
   }
-  
-  @IBInspectable
-  public var selectedColor : UIColor {
-    set(newColor) {
-      colorRing?.transform = CGAffineTransformMakeRotation(newColor.angle)
-    }
-    
-    get {
-      if let angle = colorRing!.layer.valueForKeyPath("transform.rotation.z") as? CGFloat {
-        return UIColor.colorForAngle(angle)
-      } else {
-        return UIColor.clearColor()
-      }
-    }
-  }
 }
 
-
-private extension UIColor {
-  private static func colorForAngle(angle: CGFloat) -> UIColor {
-    var normalised = (CGFloat(3 / 2.0 * M_PI) - angle) / CGFloat(2 * M_PI)
-    normalised = normalised - floor(normalised)
-    if normalised < 0 {
-      normalised += 1
-    }
-    return UIColor(hue: normalised, saturation: 1.0, brightness: 1.0, alpha: 1.0)
-  }
-  
-  private var angle : CGFloat {
-    var hue : CGFloat = 0.0
-    getHue(&hue, saturation: nil, brightness: nil, alpha: nil)
-    return hue * CGFloat(2 * M_PI)
-  }
-}
 
--- projects/08_DrawingWithCG/008_Challenge_Starter/DrawingToolbox/DrawingToolbox/ColorRing.swift
+++ projects/08_DrawingWithCG/008_Challenge_Starter/DrawingToolbox/DrawingToolbox/ColorRing.swift
@@ -25,43 +25,7 @@ import UIKit
 @IBDesignable
 class ColorRing : UIView {
   
-  private let numberSegments = 400
-  
   @IBInspectable
-  var ringWidth : CGFloat = 2.0 {
-    didSet {
-      setNeedsDisplay()
-    }
-  }
+  var ringWidth : CGFloat = 2.0
 }
 
-extension ColorRing {
-  override func drawRect(rect: CGRect) {
-    if let cxt = UIGraphicsGetCurrentContext() {
-      drawRainbowWheel(cxt, rect: rect)
-    }
-  }
-}
-
-extension ColorRing {
-  private func drawRainbowWheel(context: CGContextRef, rect: CGRect) {
-    CGContextSaveGState(context)
-    
-    let ringRadius = (min(rect.width, rect.height) - ringWidth) / 2
-    
-    CGContextSetLineWidth(context, ringWidth)
-    CGContextSetLineCap(context, .Butt)
-    
-    for segment in 0 ..< numberSegments {
-      let proportion = CGFloat(segment) / CGFloat(numberSegments)
-      let startAngle = proportion * 2 * CGFloat(M_PI)
-      let endAngle = CGFloat(segment + 1) / CGFloat(numberSegments) * 2 * CGFloat(M_PI)
-      
-      UIColor(hue: proportion, saturation: 1.0, brightness: 1.0, alpha: 1.0).setStroke()
-      CGContextAddArc(context, rect.midX, rect.midY, ringRadius, startAngle, endAngle, 0)
-      CGContextStrokePath(context)
-    }
-    
-    CGContextRestoreGState(context)
-  }
-}
